16deb300556d2a648922b632bfa1e75e1d559c52
rishabhbharadwaj1996 <rishabhbharadwaj1996@Jarvis>
2020-12-28 15:34:46 +0530

    Revert "Changed the scope of driver variable to private"
    This reverts commit 594936681e7e2475226ba480c5cf2e6f47d91511.

--- src/main/java/Factory/DriverFactory.java
+++ src/main/java/Factory/DriverFactory.java
@@ -9,7 +9,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 
 public class DriverFactory {
 
-	private WebDriver driver;
+	public WebDriver driver;
 	
 	public static ThreadLocal<WebDriver> tlDriver = new ThreadLocal<WebDriver>();
 	
